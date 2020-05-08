7c244a811215dd96148308c3c65377bf15251aa3
mishikaraj <mishikaraj99@gmail.com>
2020-05-08 12:28:45 +0530

    square root of number

---
+++ canvas.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter.ttk import *
+
+root=tk.Tk()
+
+canvas1=tk.Canvas(root,width=400,height=300)
+canvas1.pack()
+
+entry1 = tk.Entry(root)
+canvas1.create_window(200, 140,window=entry1)
+
+
+def getSquareRoot ():  
+    x1 = entry1.get()
+    label1 = tk.Label(root, text= float(x1)**0.5)
+    canvas1.create_window(200, 230, window=label1)
+    
+button1 = tk.Button(text='Get the Square Root', command=getSquareRoot)
+canvas1.create_window(200, 180, window=button1)
+
+root.mainloop()
+
+
+'''
+Wrong syntax
+root = tk.Tk()
+c=Canvas(root, bg="red", width=400, height=400)
+c.pack()
+d=Label(c, text="Hello")
+d.pack()
+root.mainloop()'''
